7fa0274af3b45d982254b46720be224799741f92
Amit-TheOne <amit.dew009@gmail.com>
2024-04-30 01:37:42 +0530

    Updated: Login and SignUp page

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./login.css";
-import { auth,provider } from "../../firebase"; 
+import { auth, provider } from "../../firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { useForm } from "react-hook-form";
 import { buttonClick } from "../../assets/functions/clickSound";
@@ -8,119 +8,129 @@ import { Icon } from "@iconify/react";
 import { signInWithPopup, updateProfile } from "firebase/auth";
 import { useDispatch } from "react-redux";
 import { loginUser } from "../../redux/reducers/userSlice";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { toastify } from "../../utils/toastify";
-// import { navigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
-  const dispatch = useDispatch();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    reset,
-  } = useForm();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
-  const loginUser = (data) => {
-    buttonClick.play();
-    signInWithEmailAndPassword(auth, data.email, data.pass)
-      .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        toastify("success",`Greetings, ${user.displayName}!`)
-        dispatch(
-          loginUser({
-            email: user.email,
-            username: user.displayName,
-            id: user.uid,
-          })
-        );
-      })
-      .catch((error) => {
-        console.log(error)
-        toastify('error',"Invalid Credentials")
-      });
-  };
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        reset,
+    } = useForm();
 
-  const googleSignIn = (e) => {
-    signInWithPopup(auth, provider)
-      .then(async (userCredential) => {
-        // Signed in
-        const user = userCredential.user;
+    const loginUser = (data) => {
+        buttonClick.play();
+        signInWithEmailAndPassword(auth, data.email, data.pass)
+            .then((userCredential) => {
+                // Signed in
+                const user = userCredential.user;
+                toastify("success", `Greetings, ${user.displayName}!`);
+                dispatch(
+                    loginUser({
+                        email: user.email,
+                        username: user.displayName,
+                        id: user.uid,
+                    })
+                );
+                reset();
+                navigate("/");
+            })
+            .catch((error) => {
+                console.log(error);
+                toastify("error", "Invalid Credentials");
+            });
+    };
 
-        console.log(user);
-        await updateProfile(user, {
-          displayName: user.displayName,
-        });
-        toastify("success",`Greetings, ${user.displayName}!`)
+    const googleSignIn = (e) => {
+        signInWithPopup(auth, provider)
+            .then(async (userCredential) => {
+                // Signed in
+                const user = userCredential.user;
 
-        dispatch(
-          loginUser({
-            email: user.email,
-            username: user.displayName,
-            id: user.uid,
-          })
-        );
+                console.log(user);
+                await updateProfile(user, {
+                    displayName: user.displayName,
+                });
+                toastify("success", `Greetings, ${user.displayName}!`);
 
-        reset();
-        // navigate('/')
+                dispatch(
+                    loginUser({
+                        email: user.email,
+                        username: user.displayName,
+                        id: user.uid,
+                    })
+                );
 
-        // ...
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        toastify("error","Some Error Occurred, Please Try Again");
-        console.log(errorCode, errorMessage);
-        // ..
-      });
-    e.preventDefault();
-  };
+                reset();
+                navigate("/");
 
-  return (
-    <div className="login">
-      <h1>
-        ZenFi <Icon className="yinyang" icon="openmoji:yin-yang" />{" "}
-        <small style={{ fontWeight: "lighter", fontSize: "20px" }}>
-          your virtual study environment
-        </small>
-      </h1>
-      <form onSubmit={handleSubmit(loginUser)}>
-        <div id="loginForm">
-          <h1 id="loginHead">Sign In</h1>
-          <input
-            id="username"
-            placeholder="Email"
-            {...register("email", { required: true })}
-          ></input>
-          <input
-            id="password"
-            type="password"
-            placeholder="Password"
-            {...register("pass", { required: true })}
-          ></input>
-          <button id="loginBtn" title="Login">
-            Login
-          </button>
-          <button id="googleBtn" onClick={googleSignIn}>
-            <Icon
-              icon="flat-color-icons:google"
-              style={{ marginRight: "5px" }}
-            />{" "}
-            Google
-          </button>
-          <span id="registerRedirect">
-            Don't have an account?{" "}
-            <a
-              style={{ textDecoration: "none", color: "#5d5c61" }}
-              href="/register"
-            >
-              Sign Up
-            </a>
-          </span>
+                // ...
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                toastify("error", "Some Error Occurred, Please Try Again");
+                console.log(errorCode, errorMessage);
+                // ..
+            });
+        e.preventDefault();
+    };
+
+    return (
+        <div className="login">
+            <h1 className="">
+                <span className="dancing-script-special"> ZenFi </span>
+                {/* <Icon className="yinyang" icon="openmoji:yin-yang" />{" "} */}
+                <small className={{ fontWeight: "lighter", fontSize: "20px" }}>
+                    virtual productivity space
+                </small>
+            </h1>
+
+            <form onSubmit={handleSubmit(loginUser)}>
+                <div id="loginForm">
+                    <h1 id="loginHead">Login</h1>
+                    <input
+                        id="email"
+                        placeholder="Email"
+                        {...register("email", { required: true })}
+                    ></input>
+                    <input
+                        id="password"
+                        type="password"
+                        placeholder="Password"
+                        {...register("pass", { required: true })}
+                    ></input>
+                    <button id="loginBtn" title="Login">
+                        Login
+                    </button>
+                    <button id="googleBtn" onClick={googleSignIn}>
+                        <Icon
+                            icon="flat-color-icons:google"
+                            style={{ marginRight: "7px" }}
+                        />{" "}
+                        Google
+                    </button>
+
+                    <span id="registerRedirect">
+                        Don&apos;t have an account?{" "}
+                        <a
+                            style={{
+                                textDecoration: "none",
+                                color: "rgb(254, 241, 241, 0.8)",
+                            }}
+                            href="/register"
+                        >
+                            Sign Up
+                        </a>
+                    </span>
+                </div>
+            </form>
         </div>
-      </form>
-    </div>
-  );
+    );
 }
--- src/pages/register/SignUp.jsx
+++ src/pages/register/SignUp.jsx
@@ -95,51 +95,60 @@ export default function SignUp() {
 
   return (
     <div className="register">
-      <h1 style={{ textAlign: "center" }}>
+      {/* <h1 style={{ textAlign: "center" }}>
         ZenFi <Icon className="yinyang" icon="openmoji:yin-yang" />{" "}
         <small style={{ fontWeight: "lighter", fontSize: "20px" }}>
           your virtual study environment
         </small>
-      </h1>
+      </h1> */}
+      <h1 className="">
+                <span className="dancing-script-special"> ZenFi </span>
+                {/* <Icon className="yinyang" icon="openmoji:yin-yang" />{" "} */}
+                <small className={{ fontWeight: "lighter", fontSize: "20px" }}>
+                    virtual productivity space
+                </small>
+            </h1>
+
       <form onSubmit={handleSubmit(registerUser)}>
-        <div id="loginForm">
-          <h1 id="loginHead">Sign Up</h1>
-          <input
-            id="username"
-            placeholder="Email"
-            {...register("email", { required: true })}
-          ></input>
+        <div id="registerForm">
+          <h1 id="registerHead">Sign Up</h1>
           <input
             id="username"
             placeholder="Username"
             {...register("username", { required: true })}
           ></input>
           <input
-            id="username"
+            id="rEmail"
+            placeholder="Email"
+            {...register("email", { required: true })}
+          ></input>
+          <input
+            id="rPassword"
             type="password"
             placeholder="Password"
             {...register("pass", { required: true })}
           ></input>
           <input
-            id="username"
+            id="confirmPassword"
             type="password"
             placeholder="Confirm Password"
             {...register("cpass", { required: true })}
           ></input>
-          <button id="loginBtn" title="Register">
+
+          <button id="registerBtn" title="Register">
             Register
           </button>
-          <button id="googleBtn" onClick={googleSignIn}>
+          <button id="rGoogleBtn" onClick={googleSignIn}>
             <Icon
               icon="flat-color-icons:google"
               style={{ marginRight: "5px" }}
             />{" "}
             Google
           </button>
-          <span id="registerRedirect">
+          <span id="loginRedirect">
             Already have an account?{" "}
-            <a style={{ textDecoration: "none", color: "#5d5c61" }} href="/">
-              Login now
+            <a style={{ textDecoration: "none", color: "rgb(254, 241, 241, 0.8)" }} href="/">
+              Login
             </a>
           </span>
         </div>
